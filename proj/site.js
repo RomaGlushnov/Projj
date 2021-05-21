9095e66ab963cd1e239c606130dd668fa00b1df9
RomaGlushnov <74004085+RomaGlushnov@users.noreply.github.com>
2021-05-21 21:48:48 +0300

    Add files via upload

---
+++ proj/site.js
@@ -0,0 +1,220 @@
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("menu");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+function Sim(sldrId) {
+
+	let id = document.getElementById(sldrId);
+	if(id) {
+		this.sldrRoot = id
+	}
+	else {
+		this.sldrRoot = document.querySelector('.slider')
+	};
+
+	// Carousel objects
+	this.sldrList = this.sldrRoot.querySelector('.slider-list');
+	this.sldrElements = this.sldrList.querySelectorAll('.slider-element');
+	this.sldrElemFirst = this.sldrList.querySelector('.slider-element');
+	this.leftArrow = this.sldrRoot.querySelector('div.slider-arrow-left');
+	this.rightArrow = this.sldrRoot.querySelector('div.slider-arrow-right');
+	this.indicatorDots = this.sldrRoot.querySelector('div.slider-dots');
+
+	// Initialization
+	this.options = Sim.defaults;
+	Sim.initialize(this)
+};
+
+Sim.defaults = {
+
+	// Default options for the carousel
+	loop: true,     // Бесконечное зацикливание слайдера
+	auto: true,     // Автоматическое пролистывание
+	interval: 5000, // Интервал между пролистыванием элементов (мс)
+	arrows: true,   // Пролистывание стрелками
+	dots: true      // Индикаторные точки
+};
+
+Sim.prototype.elemPrev = function(num) {
+	num = num || 1;
+
+	let prevElement = this.currentElement;
+	this.currentElement -= num;
+	if(this.currentElement < 0) this.currentElement = this.elemCount-1;
+
+	if(!this.options.loop) {
+		if(this.currentElement == 0) {
+			this.leftArrow.style.display = 'none'
+		};
+		this.rightArrow.style.display = 'block'
+	};
+	
+	this.sldrElements[this.currentElement].style.opacity = '1';
+	this.sldrElements[prevElement].style.opacity = '0';
+
+	if(this.options.dots) {
+		this.dotOn(prevElement); this.dotOff(this.currentElement)
+	}
+};
+
+Sim.prototype.elemNext = function(num) {
+	num = num || 1;
+	
+	let prevElement = this.currentElement;
+	this.currentElement += num;
+	if(this.currentElement >= this.elemCount) this.currentElement = 0;
+
+	if(!this.options.loop) {
+		if(this.currentElement == this.elemCount-1) {
+			this.rightArrow.style.display = 'none'
+		};
+		this.leftArrow.style.display = 'block'
+	};
+
+	this.sldrElements[this.currentElement].style.opacity = '1';
+	this.sldrElements[prevElement].style.opacity = '0';
+
+	if(this.options.dots) {
+		this.dotOn(prevElement); this.dotOff(this.currentElement)
+	}
+};
+
+Sim.prototype.dotOn = function(num) {
+	this.indicatorDotsAll[num].style.cssText = 'background-color:rgb(0, 65, 163); cursor:pointer;'
+};
+
+Sim.prototype.dotOff = function(num) {
+	this.indicatorDotsAll[num].style.cssText = 'background-color:rgb(2, 210, 238); cursor:default;'
+};
+
+Sim.initialize = function(that) {
+
+	// Constants
+	that.elemCount = that.sldrElements.length; // Количество элементов
+
+	// Variables
+	that.currentElement = 0;
+	let bgTime = getTime();
+
+	// Functions
+	function getTime() {
+		return new Date().getTime();
+	};
+	function setAutoScroll() {
+		that.autoScroll = setInterval(function() {
+			let fnTime = getTime();
+			if(fnTime - bgTime + 10 > that.options.interval) {
+				bgTime = fnTime; that.elemNext()
+			}
+		}, that.options.interval)
+	};
+
+	// Start initialization
+	if(that.elemCount <= 1) {   // Отключить навигацию
+		that.options.auto = false; that.options.arrows = false; that.options.dots = false;
+		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
+	};
+	if(that.elemCount >= 1) {   // показать первый элемент
+		that.sldrElemFirst.style.opacity = '1';
+	};
+
+	if(!that.options.loop) {
+		that.leftArrow.style.display = 'none';  // отключить левую стрелку
+		that.options.auto = false; // отключить автопркрутку
+	}
+	else if(that.options.auto) {   // инициализация автопрокрутки
+		setAutoScroll();
+		// Остановка прокрутки при наведении мыши на элемент
+		that.sldrList.addEventListener('mouseenter', function() {clearInterval(that.autoScroll)}, false);
+		that.sldrList.addEventListener('mouseleave', setAutoScroll, false)
+	};
+
+	if(that.options.arrows) {  // инициализация стрелок
+		that.leftArrow.addEventListener('click', function() {
+			let fnTime = getTime();
+			if(fnTime - bgTime > 1000) {
+				bgTime = fnTime; that.elemPrev()
+			}
+		}, false);
+		that.rightArrow.addEventListener('click', function() {
+			let fnTime = getTime();
+			if(fnTime - bgTime > 1000) {
+				bgTime = fnTime; that.elemNext()
+			}
+		}, false)
+	}
+	else {
+		that.leftArrow.style.display = 'none'; that.rightArrow.style.display = 'none'
+	};
+
+	if(that.options.dots) {  // инициализация индикаторных точек
+		let sum = '', diffNum;
+		for(let i=0; i<that.elemCount; i++) {
+			sum += '<span class="dot"></span>'
+		};
+		that.indicatorDots.innerHTML = sum;
+		that.indicatorDotsAll = that.sldrRoot.querySelectorAll('span.dot');
+		// Назначаем точкам обработчик события 'click'
+		for(let n=0; n<that.elemCount; n++) {
+			that.indicatorDotsAll[n].addEventListener('click', function() {
+				diffNum = Math.abs(n - that.currentElement);
+				if(n < that.currentElement) {
+					bgTime = getTime(); that.elemPrev(diffNum)
+				}
+				else if(n > that.currentElement) {
+					bgTime = getTime(); that.elemNext(diffNum)
+				}
+				// Если n == that.currentElement ничего не делаем
+			}, false)
+		};
+		that.dotOff(0);  // точка[0] выключена, остальные включены
+		for(let i=1; i<that.elemCount; i++) {
+			that.dotOn(i)
+		}
+	}
+};
+
+new Sim();
+
+// меню 
+
+const anchors = document.querySelectorAll('a.scroll-to')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+//кнопка наверх
+
+const anchors = document.querySelectorAll('a.scrolltop')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    const blockID = anchor.getAttribute('href')
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
